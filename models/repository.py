988c0a2c098b6f5371b91dddbd63112acbf9cde5
vaibhov22 <sonivaibhov781@gmail.com>
2026-03-16 01:31:51 +0530

    clear repository

--- models/repository.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String, ForeignKey
-from database import Base
-
-class Repository(Base):
-    __tablename__ = "repositories"
-
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String)
-    owner_id = Column(Integer, ForeignKey("users.id"))
